93d608ab0a7c4ee3b770c1af4b9ff7539efad906
firanger <jvtjuan15@gmail.com>
2021-09-04 20:10:43 -0500

    primer commit

---
+++ taller_1.py
@@ -0,0 +1,4 @@
+'''UNIVERSIDAD DE LA COSTA CUC FUNDAMENTOS ALGORÍTMICOS  TALLER DE ALGORITMOS SECUENCIALES PROFESOR: ING.  ROBERTO MORALES 
+'''
+print("todos los ejercicios estan comentados para que sea mas facil ejecutarlos uno por uno")
+'''Calcule el valor de Y indicando paso a paso como llegó al resultado '''
